d17a02800dcde1b074e5e42a7e8c056a754b1d81
tsci <tsci@gft.com>
2021-05-14 13:28:59 -0300

    first commit

---
+++ Models/Categoria.cs
@@ -0,0 +1,9 @@
+namespace Api_Macoratti.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Api_Macoratti.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
